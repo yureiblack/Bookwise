353b7307bb3311120a67001924a5700d11fcda08
yureiblack <yureiblack666@gmail.com>
2026-02-04 23:26:33 +0530

    made authMiddlware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+export const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1]
+    
+    if (!token) {
+        return res.status(400).json({message: "Not authorised"})
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch{
+        return res.status(400).json({message: "Invalid token"})
+    }
+}
+
+export default authMiddleware
