37c3bb610cc438ea81b89994d37295811943d867
yureiblack <yureiblack666@gmail.com>
2026-02-04 21:52:06 +0530

    made auth service

---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,19 @@
+import prisma from '../prisma/client.js'
+import {generateToken} from '../utils/jwt.js'
+
+const authWithEmail = async (email) => {
+    let user = await prisma.user.findUnique({
+        where: {email}
+    })
+
+    if(!user){
+        user = await prisma.user.create({
+            data: {email}
+        })
+    }
+
+    const token = generateToken({userId: user.id})
+    return {user, token}
+}
+
+export default authWithEmail
