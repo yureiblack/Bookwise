44723594475085bf999774eb6cc866d172e5efa6
yureiblack <yureiblack666@gmail.com>
2026-02-07 23:54:20 +0530

    added homepage in page.js

---
+++ frontend/src/app/page.js
@@ -0,0 +1,12 @@
+'use client'
+import Navbar from '../../components/Navbar'
+import HeroSection from '../../components/HeroSection'
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  )
+}
